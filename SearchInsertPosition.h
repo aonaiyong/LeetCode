e6f63dcfce96c3246c10af0389411782a8233d56
aonaiyong <aonaiyong@gmail.com>
2014-09-19 17:05:41 -0400

    update

--- SearchInsertPosition.h
+++ SearchInsertPosition.h
@@ -10,12 +10,21 @@
  You may assume no duplicates in the array.
 
  Here are few examples.
- [1,3,5,6], 5 → 2
- [1,3,5,6], 2 → 1
- [1,3,5,6], 7 → 4
- [1,3,5,6], 0 → 0
+ [1,3,5,6], 5 -> 2
+ [1,3,5,6], 2 -> 1
+ [1,3,5,6], 7 -> 4
+ [1,3,5,6], 0 -> 0
 
- Solution: 1. 2-branch binary search.
+ Solution:    1. 1-branch BS.
+              loop invariant:   |    <    |      =?      |   >=   |
+                                 0         low        top      n-1
+
+              termination:      |       <       |=?|      >=      |
+                                 0              low            n-1
+                                               (top)
+                  note that low is in range [0,n-1]
+
+              2. 2-branch BS.
               loop invariant:   |    <    |      =?      |    >   |
                                  0         low        top      n-1
 
@@ -25,15 +34,6 @@
                                 |         <      |        >       |
                                  0            top low          n-1
                   note that [top,low] could be [-1,0] or [n-1,n]
-
-           2. 1-branch binary search.
-              loop invariant:   |    <    |      =?      |   >=   |
-                                 0         low        top      n-1
-
-              termination:      |       <       |=?|      >=      |
-                                 0              low            n-1
-                                               (top)
-                  note that low is in range [0,n-1]
  */
 
 #ifndef SEARCHINSERTPOSITION_H_
@@ -43,6 +43,23 @@ class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
         return searchInsert1B(A, n, target);
+    }
+
+    // 1 branch & iterative
+    int searchInsert1B(int A[], int n, int target) {
+        int low = 0, top = n - 1;
+        while (low < top) {
+            int mid = low + (top - low) / 2;
+            if (A[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                top = mid;
+            }
+        }
+
+        if (A[low] < target) return low + 1;
+        return low;
     }
 
     // 2 branches & iterative
@@ -64,22 +81,6 @@ public:
         return low;
     }
 
-    // 1 branch & iterative
-    int searchInsert1B(int A[], int n, int target) {
-        int low = 0, top = n - 1;
-        while (low < top) {
-            int mid = low + (top - low) / 2;
-            if (A[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                top = mid;
-            }
-        }
-
-        if (A[low] < target) return low + 1;
-        return low;
-    }
 };
 
 #endif /* SEARCHINSERTPOSITION_H_ */
