4b13c6e8721fa61169d67003a3b14ea835dcac95
aonaiyong <aonaiyong@gmail.com>
2014-09-19 14:15:26 -0400

    first commit

---
+++ SearchInsertPosition.h
@@ -0,0 +1,17 @@
+/*
+ * Author:     naiyong, aonaiyong@gmail.com
+ * Date:       Sep 19, 2014
+ * Problem:    SearchInsertPosition.h
+ * Difficulty: TODO
+ * Source:     TODO
+ * Notes:	   TODO
+ * Solution:   TODO
+ */
+
+#ifndef SEARCHINSERTPOSITION_H_
+#define SEARCHINSERTPOSITION_H_
+
+
+
+
+#endif /* SEARCHINSERTPOSITION_H_ */
