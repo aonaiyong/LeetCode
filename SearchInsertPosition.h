654247d6a5d7fc74a53763b98d376e7e16581157
aonaiyong <aonaiyong@gmail.com>
2014-09-19 16:18:50 -0400

    update

--- SearchInsertPosition.h
+++ SearchInsertPosition.h
@@ -22,8 +22,8 @@
               termination:      |    <      |    =    |     >     |
                                  0           low   top         n-1
 
-                                |    <            |       >       |
-                                 0         top low             n-1
+                                |    <          |       >       |
+                                 0           top low         n-1
 
            2. 1-branch binary search.
               loop invariant:   |    <    |      =?      |   >=   |
