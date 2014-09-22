5c357c188493981063f6a38123d56d438e2a545b
aonaiyong <aonaiyong@gmail.com>
2014-09-21 23:20:03 -0400

    final version

---
+++ RotateList.h
@@ -0,0 +1,55 @@
+/*
+ Author:     naiyong, aonaiyong@gmail.com
+ Date:       Sep 21, 2014
+ Problem:    Rotate List
+ Difficulty: 3
+ Source:     https://oj.leetcode.com/problems/rotate-list/
+ Notes:
+ Given a list, rotate the list to the right by k places, where k is non-negative.
+
+ For example:
+ Given 1->2->3->4->5->nullptr and k = 2,
+ return 4->5->1->2->3->nullptr.
+
+ Solution:   Two pointers.
+ */
+
+#ifndef ROTATELIST_H_
+#define ROTATELIST_H_
+
+// Definition for singly-linked list.
+struct ListNode {
+	int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if (!head) return nullptr;
+
+        // find tail node and calculate size of list
+        ListNode *tail = head;
+        int size = 1;
+        while (tail->next) {
+            ++size;
+            tail = tail->next;
+        }
+        k %= size;
+        if (!k) return head;
+
+        // find the (k+1)st node from the end of list
+        ListNode *curr = head;
+        for (int i = 0; i < size - k - 1; ++i) {
+            curr = curr->next;
+        }
+        tail->next = head;
+        head = curr->next;
+        curr->next = nullptr;
+
+        return head;
+    }
+};
+
+#endif /* ROTATELIST_H_ */
