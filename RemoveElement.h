f62747a7484f6e1c090db2388903aa19fe2ac710
aonaiyong <aonaiyong@gmail.com>
2014-09-21 21:09:18 -0400

    First Commit

---
+++ RemoveElement.h
@@ -0,0 +1,33 @@
+/*
+ Author:     naiyong, aonaiyong@gmail.com
+ Date:       Sep 21, 2014
+ Problem:    Remove Element
+ Difficulty: 1
+ Source:     https://oj.leetcode.com/problems/remove-element/
+ Notes:
+
+ Given an array and a value, remove all instances of that value in place and return the new length.
+
+ The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+ Solution:  Two pointers.
+ */
+
+#ifndef REMOVEELEMENT_H_
+#define REMOVEELEMENT_H_
+
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        int i = -1;
+        for (int j = 0; j < n; ++j) {
+            if (A[j] != elem) {
+                A[++i] = A[j];
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+#endif /* REMOVEELEMENT_H_ */
