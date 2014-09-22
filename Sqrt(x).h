4415362ee153e5694d49e075531e5b06a1a9d621
aonaiyong <aonaiyong@gmail.com>
2014-09-22 15:33:09 -0400

    final version

---
+++ Sqrt(x).h
@@ -0,0 +1,59 @@
+/*
+ Author:     naiyong, aonaiyong@gmail.com
+ Date:       Sep 22, 2014
+ Problem:    Sqrt(x)
+ Difficulty: 4
+ Source:     https://github.com/leetcoders/LeetCode
+ Notes:
+ Implement int sqrt(int x).
+
+ Compute and return the square root of x.
+
+ Solution:   1. Newton's method. http://en.wikipedia.org/wiki/Newton%27s_method
+             2. Binary search in range [0, x].
+ */
+
+#ifndef SQRT_X__H_
+#define SQRT_X__H_
+
+class Solution {
+public:
+    int sqrt(int x) {
+        return sqrtNewtonsMethod(x);
+        // return sqrtBinarySearch(x);
+    }
+
+    int sqrtNewtonsMethod(int x) {
+        if (x == 0) return 0;
+
+        double prev = 0, curr = 1;
+        while (curr != prev) {
+            prev = curr;
+            curr = (curr + x / curr) / 2;
+        }
+
+        return static_cast<int>(curr);
+    }
+
+    int sqrtBinarySearch(int x) {
+        long long low = 0;
+        long long top = x;
+        while (low <= top) {
+            long long mid = low + (top - low) / 2;
+            long long square = mid * mid;
+            if (square == x) {
+                return mid;
+            }
+            else if (square < x) {
+                low = mid + 1;
+            }
+            else {
+                top = mid - 1;
+            }
+        }
+
+        return static_cast<int>(top);
+    }
+};
+
+#endif /* SQRT_X__H_ */
