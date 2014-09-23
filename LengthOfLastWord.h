f20dacc696e97e120a9a4ca4c6be7d120cceb4f4
aonaiyong <aonaiyong@gmail.com>
2014-09-23 01:01:39 -0400

    final version

---
+++ LengthOfLastWord.h
@@ -0,0 +1,40 @@
+/*
+ Author:     naiyong, aonaiyong@gmail.com
+ Date:       Sep 23, 2014
+ Problem:    LengthOfLastWord.h
+ Difficulty: 1
+ Source:     https://oj.leetcode.com/problems/length-of-last-word/
+ Notes:
+ Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+ If the last word does not exist, return 0.
+
+ Note: A word is defined as a character sequence consists of non-space characters only.
+
+ For example,
+ Given s = "Hello World",
+ return 5.
+
+ Solution:   Note that trailing space should be ignored.
+ */
+
+#ifndef LENGTHOFLASTWORD_H_
+#define LENGTHOFLASTWORD_H_
+
+#include <cstring>
+
+class Solution {
+public:
+    int lengthOfLastWord(const char *s) {
+        int len = strlen(s);
+        int ret = 0;
+        for (int i = len - 1; i >= 0; --i) {
+            if (s[i] != ' ') ++ret;
+            else if (ret) break;    // ignore trailing space
+        }
+
+        return ret;
+    }
+};
+
+#endif /* LENGTHOFLASTWORD_H_ */
