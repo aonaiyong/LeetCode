4803e1aeaef53f39c235964405e1003812dc7317
aonaiyong <aonaiyong@gmail.com>
2014-09-22 23:04:29 -0400

    final version

--- FourSum.h
+++ FourSum.h
@@ -1,12 +1,23 @@
 /*
  Author:     naiyong, aonaiyong@gmail.com
  Date:       Sep 22, 2014
- Problem:    FourSum.h
- Difficulty:
- Source:
+ Problem:    4Sum
+ Difficulty: 3
+ Source:     https://oj.leetcode.com/problems/4sum/
  Notes:
+ Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
 
- Solution:
+ Note:
+ Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+ The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+
+ Solution:   3SUM. http://en.wikipedia.org/wiki/3SUM
  */
 
 #ifndef FOURSUM_H_
