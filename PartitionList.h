aadc54d952b686f81dddfbc7c7a5a25592badb96
aonaiyong <aonaiyong@gmail.com>
2014-09-23 20:13:23 -0400

    final version

--- PartitionList.h
+++ PartitionList.h
@@ -22,14 +22,6 @@
 
 #include "ListNode.h"
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
