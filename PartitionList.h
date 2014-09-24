08b4d77599b65c0f3a2add4a0d264d7b4ac90ba2
aonaiyong <aonaiyong@gmail.com>
2014-09-23 20:12:08 -0400

    final version

--- PartitionList.h
+++ PartitionList.h
@@ -13,7 +13,8 @@
  Given 1->4->3->2->5->2 and x = 3,
  return 1->2->2->4->3->5.
 
- Solution:   Two pointers.
+ Solution:   1. First decouple nodes into two lists, and then splice the lists.
+             2. Partition in list.
  */
 
 #ifndef PARTITIONLIST_H_
@@ -21,11 +22,44 @@
 
 #include "ListNode.h"
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-        if (!head || !head->next) return head;
+        return partitionDecouple(head, x);
+    }
 
+    // succinct but maybe less efficient
+    ListNode *partitionDecouple(ListNode *head, int x) {
+        ListNode leftDummy(0), rightDummy(0);
+        ListNode *left = &leftDummy, *right = &rightDummy;
+        // decouple nodes < x and nodes >= x, respectively
+        while (head) {
+            if (head->val < x) {
+                left = left->next = head;
+            }
+            else {
+                right = right->next = head;
+            }
+
+            head = head->next;
+        }
+
+        // splice the two lists
+        left->next = rightDummy.next;
+        right->next = nullptr;
+        return leftDummy.next;
+    }
+
+    // verbose
+    ListNode *partitionInPlace(ListNode *head, int x) {
         ListNode dummy(0);
         dummy.next = head;
         ListNode *less = &dummy, *greater = &dummy;
