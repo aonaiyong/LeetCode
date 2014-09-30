924ee941a390ce5094a8272e94c9dbf2e8a66fa1
aonaiyong <aonaiyong@gmail.com>
2014-09-29 20:56:54 -0400

    final version

--- MaximumDepthOfBinaryTree.h
+++ MaximumDepthOfBinaryTree.h
@@ -9,8 +9,12 @@
 
  The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
- Solution:   Recursion.
-             Time: O(n), Space: O(n).
+ Solution:   1. Recursive Post-order Traversal.
+                Time: O(n), Space: O(n).
+             2. Iterative Post-order Traversal.
+                Time: O(n), Space: O(n).
+             3. Iterative Level-order Traversal.
+                Time: O(n), Space: O(n).
  */
 
 #ifndef MAXIMUMDEPTHOFBINARYTREE_H_
@@ -19,13 +23,73 @@
 #include <algorithm>
 using std::max;
 
+#include <stack>
+using std::stack;
+
+#include <queue>
+using std::queue;
+
 #include "TreeNode.h"
 
 class Solution {
 public:
     int maxDepth(TreeNode *root) {
+        return iterativeQueueMaxDepth(root);
+    }
+
+    int recursiveMaxDepth(TreeNode *root) {
         if (!root) return 0;
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        int leftDepth = recursiveMaxDepth(root->left);
+        int rightDepth = recursiveMaxDepth(root->right);
+        return 1 + max(leftDepth, rightDepth);
+    }
+
+    int iterativeStackMaxDepth(TreeNode *root) {
+        int maxDepth = 0;
+        stack<TreeNode *> stk;
+        TreeNode *lastNodeVisited = nullptr;
+        while (root || !stk.empty()) {
+            if (root) {
+                stk.push(root);
+                root = root->left;
+            }
+            else {
+                TreeNode *peakNode = stk.top();
+                if (!peakNode->right || peakNode->right == lastNodeVisited) {
+                    if (!peakNode->right && !peakNode->left) {
+                        if (maxDepth < stk.size()) maxDepth = stk.size();
+                    }
+
+                    lastNodeVisited = peakNode;
+                    stk.pop();
+                }
+                else {
+                    root = peakNode->right;
+                }
+            }
+        }
+
+        return maxDepth;
+    }
+
+    int iterativeQueueMaxDepth(TreeNode *root) {
+        int maxDepth = 0;
+        queue<TreeNode *> frontier;
+        if (root) frontier.push(root);
+        while (!frontier.empty()) {
+            ++maxDepth;
+
+            int n = frontier.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode *node = frontier.front();
+                if (node->left) frontier.push(node->left);
+                if (node->right) frontier.push(node->right);
+
+                frontier.pop();
+            }
+        }
+
+        return maxDepth;
     }
 };
 
