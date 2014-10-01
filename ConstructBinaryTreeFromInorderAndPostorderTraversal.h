dec00ac4de80e3fa7f71243b14ea090ef18f8fdc
aonaiyong <aonaiyong@gmail.com>
2014-10-01 08:41:00 -0400

    final version

--- ConstructBinaryTreeFromInorderAndPostorderTraversal.h
+++ ConstructBinaryTreeFromInorderAndPostorderTraversal.h
@@ -30,10 +30,10 @@ using std::iterator;
 class Solution {
 public:
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-        return recursiveBuildTree(inorder.cbegin(), postorder.cbegin(), inorder.size());
+        return buildTree(inorder.cbegin(), postorder.cbegin(), inorder.size());
     }
 
-    TreeNode *recursiveBuildTree(vector<int>::const_iterator ibeg, vector<int>::const_iterator pbeg, int n) {
+    TreeNode *buildTree(vector<int>::const_iterator ibeg, vector<int>::const_iterator pbeg, int n) {
         if (n <= 0) return nullptr;
 
         int val = pbeg[n-1];
@@ -41,8 +41,8 @@ public:
         while (ibeg[mid] != val) ++mid;
 
         TreeNode *root = new TreeNode(val);
-        root->left = recursiveBuildTree(ibeg, pbeg, mid);
-        root->right = recursiveBuildTree(ibeg + mid + 1, pbeg + mid, n - mid - 1);
+        root->left = buildTree(ibeg, pbeg, mid);
+        root->right = buildTree(ibeg + mid + 1, pbeg + mid, n - mid - 1);
         return root;
     }
 };
