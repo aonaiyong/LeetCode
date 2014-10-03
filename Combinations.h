1a670aaf1b1711011f8a276cc7f734ffa952460e
aonaiyong <aonaiyong@gmail.com>
2014-10-03 15:44:34 -0400

    final version

---
+++ Combinations.h
@@ -0,0 +1,55 @@
+/*
+ Author:     naiyong, aonaiyong@gmail.com
+ Date:       Oct 3, 2014
+ Problem:    Combinations
+ Difficulty: 3
+ Source:     https://oj.leetcode.com/problems/combinations/
+ Notes:
+ Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+ For example,
+ If n = 4 and k = 2, a solution is:
+
+ [
+   [2,4],
+   [3,4],
+   [2,3],
+   [1,2],
+   [1,3],
+   [1,4],
+ ]
+
+ Solution:   Depth-first search.
+ */
+
+#ifndef COMBINATIONS_H_
+#define COMBINATIONS_H_
+
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int> > combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> combination;
+        combine(n, k, 1, combination, result);
+
+        return result;
+    }
+
+    void combine(int n, int k, int start, vector<int> &combination, vector<vector<int>> &result) {
+        if (k == 0) {
+            result.push_back(combination);
+            return;
+        }
+
+        for (int i = start; i <= n-k+1; ++i) {
+            combination.push_back(i);
+            combine(n, k-1, i+1, combination, result);
+            combination.pop_back();
+        }
+    }
+};
+
+#endif /* COMBINATIONS_H_ */
