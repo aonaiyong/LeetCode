06e116a50599a599711419785f5e29c28becd812
aonaiyong <aonaiyong@gmail.com>
2014-10-03 22:21:01 -0400

    final version

--- Subsets.h
+++ Subsets.h
@@ -37,18 +37,21 @@ class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
         sort(S.begin(), S.end());
-
         vector<vector<int>> power;
         vector<int> sub;
-        for (int k = 0; k <= S.size(); ++k) {
-            subsets(S, k, 0, sub, power);
-        }
 
-        // subsets(S, 0, sub, power);
+        // for (int k = 0; k <= S.size(); ++k) { // recursive solution 1
+        //     subsets(S, k, 0, sub, power);
+        // }
+
+        // subsets(S, 0, sub, power); // recursive solution 2
+
+        subsets(S, power);  // iterative solution
 
         return power;
     }
 
+    // recursive solution 1: for subsets of size k
     void subsets(const vector<int> &S, int k, int pos, vector<int> &sub, vector<vector<int>> &power) {
         if (k == 0) {
             power.push_back(sub);
@@ -62,12 +65,25 @@ public:
         }
     }
 
+    // recursive solution 2: for all subsets
     void subsets(const vector<int> &S, int pos, vector<int> &sub, vector<vector<int>> &power) {
         power.push_back(sub);
         for (int i = pos; i < S.size(); ++i) {
             sub.push_back(S[i]);
             subsets(S, i+1, sub, power);
             sub.pop_back();
+        }
+    }
+
+    // iterative solution: merge each element of S into power respectively
+    void subsets(const vector<int> &S, vector<vector<int>> &power) {
+        power.resize(1); // initially empty
+        for (int i = 0; i < S.size(); ++i) {
+            int n = power.size();
+            for (int j = 0; j < n; ++j) {
+                power.push_back(power[j]);
+                power.back().push_back(S[i]);
+            }
         }
     }
 };
