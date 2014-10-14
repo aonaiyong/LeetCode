cd205aac3eb2b206d8eb3735e6a302c93d381afe
aonaiyong <aonaiyong@gmail.com>
2014-10-14 20:44:34 +0800

    final version

--- MergeTwoSortedLists.h
+++ MergeTwoSortedLists.h
@@ -8,6 +8,7 @@
  Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
  Solution:   Use dummy node.
+             Time: O(|l1|+|l2|), Space: O(1)
  */
 
 #ifndef MERGETWOSORTEDLISTS_H_
