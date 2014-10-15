7de8979e2335a67a95677ccb46f0aa95db68426a
aonaiyong <aonaiyong@gmail.com>
2014-10-15 22:28:08 +0800

    final version

--- InsertionSortArray.h
+++ InsertionSortArray.h
@@ -41,12 +41,12 @@ class Solution {
 public:
     void insertionSortArray(int A[], int n) {
         for (int i = 1; i < n; ++i) {
-        	int key = A[i];
-        	int j = i - 1;
-        	while (j >= 0 && A[j] > key) {
-        		A[j+1] = A[j];
-        		--j;
-        	}
+            int key = A[i];
+            int j = i - 1;
+            while (j >= 0 && A[j] > key) {
+                A[j+1] = A[j];
+                --j;
+            }
         	A[j+1] = key;
         }
     }
