e8606a80656f10e6f2e9789d590ede023a4fc8a1
aonaiyong <aonaiyong@gmail.com>
2014-10-15 21:19:43 +0800

    final version

--- QuickSort3.h
+++ QuickSort3.h
@@ -48,68 +48,68 @@ using std::swap;
 using std::pair; using std::make_pair;
 
 class Solution {
-	void quickSort3(int A[], int p, int r) {
-		if (p < r) {
-			int x = A[pivot(A, p, r)];
-			pair<int, int> q = partition3(A, p, r, x, x);
-			quickSort3(A, p, q.first);
-			quickSort3(A, q.second, r);
+    void quickSort3(int A[], int p, int r) {
+	    if (p < r) {
+		    int x = A[pivot(A, p, r)];
+		    pair<int, int> q = partition3(A, p, r, x, x);
+		    quickSort3(A, p, q.first);
+		    quickSort3(A, q.second, r);
 		}
 	}
 
-	void quickSort3Iterative(int A[], int p, int r) {
-		stack<pair<int, int>> stk;
-		if (p < r)
-		    stk.push(make_pair(p, r));
-		while (!stk.empty()) {
-			p = stk.top().first;
-			r = stk.top().second;
-			stk.pop();
+    void quickSort3Iterative(int A[], int p, int r) {
+	    stack<pair<int, int>> stk;
+	    if (p < r)
+	        stk.push(make_pair(p, r));
+	    while (!stk.empty()) {
+		    p = stk.top().first;
+		    r = stk.top().second;
+		    stk.pop();
 
-			int x = A[pivot(A, p, r)];
-			pair<int, int> q = partition3(A, p, r, x, x);
-			if (q.second < r)
+		    int x = A[pivot(A, p, r)];
+		    pair<int, int> q = partition3(A, p, r, x, x);
+		    if (q.second < r)
 			    stk.push(make_pair(q.second, r));
-			if (p < q.first)
+		    if (p < q.first)
 			    stk.push(make_pair(p, q.first));
-		}
-	}
+	    }
+    }
 
-	// 3-way partition
-	pair<int, int> partition3(int A[], int p, int r, int mid, int high) {
-		int i = p-1, j = p, k = r + 1;
-		while (j < k) {
-			if (A[j] < mid)
-				swap(A[++i], A[j++]);
-			else if (A[j] > high)
+    // 3-way partition
+    pair<int, int> partition3(int A[], int p, int r, int mid, int high) {
+	    int i = p-1, j = p, k = r + 1;
+	    while (j < k) {
+		    if (A[j] < mid)
+			    swap(A[++i], A[j++]);
+		    else if (A[j] > high)
 			    swap(A[j], A[--k]);
-			else
+		    else
 			    ++j;
-		}
-		return make_pair(i, j);
-	}
+	    }
+	    return make_pair(i, j);
+    }
 
-	int pivot(int A[], int p, int r) {
-		return median(A, p, r);
-	}
+    int pivot(int A[], int p, int r) {
+	    return median(A, p, r);
+    }
 
-	// median of the first, middle and last element (2-3 comparisons)
-	int median(int A[], int p, int r) {
-		int q = p + (r - p) / 2;
-		if (A[p] < A[q]) {
-			if (A[p] > A[r])       // (A[r], A[p], A[q])
+    // median of the first, middle and last element (2-3 comparisons)
+    int median(int A[], int p, int r) {
+	    int q = p + (r - p) / 2;
+	    if (A[p] < A[q]) {
+		    if (A[p] > A[r])       // (A[r], A[p], A[q])
 			    return p;
-			else if (A[q] < A[r])  // (A[p], A[q], A[r])
+		    else if (A[q] < A[r])  // (A[p], A[q], A[r])
 			    return q;
-		}
-		else {
-			if (A[q] > A[r])       // (A[r], A[q], A[p])
+	    }
+	    else {
+		    if (A[q] > A[r])       // (A[r], A[q], A[p])
 			    return q;
-			else if (A[p] < A[r])  // (A[q], A[p], A[r])
+		    else if (A[p] < A[r])  // (A[q], A[p], A[r])
 			    return p;
-		}
-		return r;                 // (A[p], A[r], A[q]) or (A[q], A[r], A[p])
-	}
+	    }
+	    return r;                 // (A[p], A[r], A[q]) or (A[q], A[r], A[p])
+    }
 };
 
 #endif /* QUICKSORT3_H_ */
