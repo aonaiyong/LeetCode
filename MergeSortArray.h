d75a3df072934354151f3fe8e0d63e0c69e4d224
aonaiyong <aonaiyong@gmail.com>
2014-10-16 18:44:10 +0800

    final version

--- MergeSortArray.h
+++ MergeSortArray.h
@@ -53,41 +53,41 @@ using std::min;
 
 class Solution {
 public:
-	void mergeSort(int A[], int n) {
-		int *B = new int[n/2];
-		// topDownMergeSort(A, 0, n - 1, B);
-		bottomUpMergeSort(A, n, B);
 
-		delete[] B;
-	}
+    void mergeSort(int A[], int n) {
+        int *B = new int[n/2];
+        // topDownMergeSort(A, 0, n - 1, B);
+	    bottomUpMergeSort(A, n, B);
 
-	void bottomUpMergeSort(int A[], int n, int B[]) {
-		// width denotes subset size
-		for (int width = 1; width < n; width <<= 1) {
-			// Merge consecutive subsets
-			for (int i = 0; i < n; i += 2 * width)
-				merge(A, i, min(i+width, n) - 1, min(i+2*width, n) - 1, B);
-		}
-	}
+	    delete[] B;
+    }
 
-	// Both p and r are inclusive
-	void topDownMergeSort(int A[], int p, int r, int B[]) {
-		if (p < r) {                          // Initial range A[p...r], where p < r
-			int q = p + (r - p) / 2;          // p <= q < r
-			topDownMergeSort(A, p, q, B);     // Reduced range A[p...q]
-			topDownMergeSort(A, q + 1, r, B); // Reduced range A[q+1...r]
-			merge(A, p, q, r, B);             // Merge sorted array A[p...q] and A[q+1...r]
-		}
-	}
+    void bottomUpMergeSort(int A[], int n, int B[]) {
+        // width denotes subset size
+        for (int width = 1; width < n; width <<= 1)
+            // Merge consecutive subsets
+            for (int i = 0; i < n; i += 2 * width)
+                merge(A, i, min(i+width, n) - 1, min(i+2*width, n) - 1, B);
+    }
 
-	// Merge two sorted array A[p...q] and A[q+1...r]
-	// Note that A[q+1...r] might be empty (r <= q)
-	void merge(int A[], int p, int q, int r, int B[]) {
-		int m = r-q-1;           // m is inclusive
-		// Copy A[q+1...r] into B[0...m]
-		for (int i = 0; i <= m; ++i) {
-			B[i] = A[i + q + 1];
-		}
+    // Both p and r are inclusive
+    void topDownMergeSort(int A[], int p, int r, int B[]) {
+        if (p < r) {                          // Initial range A[p...r], where p < r
+            int q = p + (r - p) / 2;          // p <= q < r
+            topDownMergeSort(A, p, q, B);     // Reduced range A[p...q]
+            topDownMergeSort(A, q + 1, r, B); // Reduced range A[q+1...r]
+		    merge(A, p, q, r, B);             // Merge sorted array A[p...q] and A[q+1...r]
+	    }
+    }
+
+    // Merge two sorted array A[p...q] and A[q+1...r]
+    // Note that A[q+1...r] might be empty (r <= q)
+    void merge(int A[], int p, int q, int r, int B[]) {
+        int m = r-q-1;           // m is inclusive
+        // Copy A[q+1...r] into B[0...m]
+        for (int i = 0; i <= m; ++i) {
+            B[i] = A[i + q + 1];
+        }
 
 		// Merge A[p...q] and B[0...m]
 		int i = q, j = m, k = r;  // i, j, and k are all inclusive
