638642fe7865ec22cc945be57596c8b22168fe0a
aonaiyong <aonaiyong@gmail.com>
2014-10-21 10:23:14 +0800

    final version

--- ShortestCommonSupersequence.h
+++ ShortestCommonSupersequence.h
@@ -25,15 +25,15 @@ using std::min;
 
 class Solution {
 public:
-	// Time: O(|x|*|y|), Space: O(|x|*|y|)
-	int SCSLength(const string &x, const string &y, vector<vector<int> > &DP) {
-		int m = x.size(), n = y.size();
-		for (int i = 0; i <= m; ++i)
-			DP[i][0] = i;
-		for (int j = 0; j <= n; ++j)
-			DP[0][j] = j;
+    // Time: O(|x|*|y|), Space: O(|x|*|y|)
+    int SCSLength(const string &x, const string &y, vector<vector<int> > &DP) {
+        int m = x.size(), n = y.size();
+        for (int i = 0; i <= m; ++i)
+            DP[i][0] = i;
+        for (int j = 0; j <= n; ++j)
+            DP[0][j] = j;
 
-		for (int i = 1; i <= m; ++i) {
+        for (int i = 1; i <= m; ++i) {
 			for (int j = 1; j <= n; ++j)
 				if (x[i-1] == y[j-1])
 					DP[i][j] = DP[i-1][j-1] + 1;
