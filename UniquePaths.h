0cea6437b7d99b47f21e6b94a81caaef9a47ace5
aonaiyong <aonaiyong@gmail.com>
2014-10-22 20:19:52 +0800

    final version

--- UniquePaths.h
+++ UniquePaths.h
@@ -23,12 +23,25 @@ using std::vector;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        return uniquePathsLinear(m, n);
+    }
+
+    int uniquePathsQuadratic(int m, int n) {
         vector<vector<int> > DP(m, vector<int>(n, 1));
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j)
                 DP[i][j] = DP[i][j-1] + DP[i-1][j];
         }
         return DP[m-1][n-1];
+    }
+
+    int uniquePathsLinear(int m, int n) {
+        vector<int> DP(n, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j)
+                DP[j] += DP[j-1];
+        }
+        return DP[n-1];
     }
 };
 
