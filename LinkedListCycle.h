b7abbec48cd039c0b55d6d6c78b61562c666ed25
aonaiyong <aonaiyong@gmail.com>
2014-10-29 08:41:02 +0800

    final version

--- LinkedListCycle.h
+++ LinkedListCycle.h
@@ -20,17 +20,19 @@
 
 class Solution {
 public:
-	bool hasCycle(ListNode *head) {
-		if (!head || !head->next) return false;
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next)
+            return false;
 
-         ListNode *slow = head, *fast = head;
-         while (fast && fast->next) {
-             slow = slow->next;
-             fast = fast->next->next;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
 
-             if (slow == fast) return true;
+            if (slow == fast)
+                return true;
          }
-         return false;
+        return false;
      }
  };
 
--- LinkedListCycleII.h
+++ LinkedListCycleII.h
@@ -21,24 +21,26 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       if (!head || !head->next) return nullptr;
+        if (!head || !head->next) return nullptr;
 
-       ListNode *slow = head, *fast = head;
-       while (fast && fast->next) {
-           slow = slow->next;
-           fast = fast->next->next;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
 
-           if (slow == fast) break;
-       }
-       if (slow != fast) return nullptr;
+            if (slow == fast)
+                break;
+        }
+        if (slow != fast)
+            return nullptr;
 
-       fast = head;
-       while (fast != slow) {
-           fast = fast->next;
-           slow = slow->next;
-       }
+        fast = head;
+        while (fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
 
-       return slow;
+        return slow;
     }
 };
 
