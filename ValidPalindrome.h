c3e42a6463a9efdcf6654205cc266660734f3afa
aonaiyong <aonaiyong@gmail.com>
2014-10-31 15:28:02 +0800

    final version

--- ValidPalindrome.h
+++ ValidPalindrome.h
@@ -32,14 +32,15 @@ using std::isalnum; using std::tolower;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0, j = s.size() - 1;
-        while (i < j) {
-            while (i < j && !isalnum(s[i])) ++i;
-            while (i < j && !isalnum(s[j])) --j;
+        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
+            while (i < j && !isalnum(s[i]))
+                ++i;
+            while (i < j && !isalnum(s[j]))
+                --j;
 
-            if (tolower(s[i++]) != tolower(s[j--])) return false;
+            if (tolower(s[i]) != tolower(s[j]))
+                return false;
         }
-
         return true;
     }
 };
