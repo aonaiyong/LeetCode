a189c6342c1d7e1535cf88cb6c30f9a702413243
aonaiyong <aonaiyong@gmail.com>
2014-11-01 18:14:29 +0800

    final version

--- SearchIncreasingDecreasingArray.h
+++ SearchIncreasingDecreasingArray.h
@@ -49,6 +49,7 @@ public:
 
      // search in A[low...top] for target,
      // return its index if present or -1 if not.
+     // A[low...top] is either sorted or reverse sorted.
      template <typename Compare>
      int binarySearch(int A[], int low, int top, int target, Compare comp) {
          while (low < top) {
