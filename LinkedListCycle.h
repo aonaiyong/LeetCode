675c4b3fc32429f77bb6a25859f8822807bca1b6
aonaiyong <aonaiyong@gmail.com>
2014-11-02 18:51:39 +0800

    final version

--- LinkedListCycle.h
+++ LinkedListCycle.h
@@ -21,9 +21,6 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head || !head->next)
-            return false;
-
         ListNode *slow = head, *fast = head;
         while (fast && fast->next) {
             slow = slow->next;
