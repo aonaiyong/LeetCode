c480cc02ae0edd8819b35acaaecdcb072b9393f3
aonaiyong <aonaiyong@gmail.com>
2014-11-03 20:32:45 +0800

    final version

--- StringMatchingContentLength.h
+++ StringMatchingContentLength.h
@@ -31,36 +31,26 @@ public:
     int SMCLength(const string &x, const string &y) {
         int m = x.size(), n = y.size();
         VEC2D DP(m+1, VEC1D(n+1));
-        VEC2D cnt(m+1, VEC1D(n+1));
-        for (int i = 1; i <= m; ++i)
+        for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j)
                 if (x[i-1] == y[j-1])
                     DP[i][j] = DP[i-1][j-1] + 1;
                 else
                     DP[i][j] = 0;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j)
-                cout << DP[i][j] << "  ";
-            cout << endl;
         }
-        cout << endl << endl;
 
         int largest = 0;
+        VEC2D cnt(m+1, VEC1D(n+1));
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 int len = DP[i][j];
-                if (len >= 3)
+                if (len >= 3) {
                     cnt[i][j] = len + cnt[i-len][j-len];
+                }
                 else
                     cnt[i][j] = max(max(cnt[i-1][j], cnt[i][j-1]), cnt[i-1][j-1]);
                 largest = max(largest, cnt[i][j]);
             }
-        }
-
-        for (int i = 1; i <= m; ++i) {
-              for (int j = 1; j <= n; ++j)
-                  cout << cnt[i][j] << "  ";
-              cout << endl;
         }
 
         return largest;
