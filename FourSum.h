10b9b17dbe3da94c2fe75c08b0e762d0e9d0a060
aonaiyong <aonaiyong@gmail.com>
2014-11-07 22:23:56 +0800

    final version

--- FourSum.h
+++ FourSum.h
@@ -8,7 +8,7 @@
  Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
 
  Note:
- Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+ Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a 鈮�b 鈮�c 鈮�d)
  The solution set must not contain duplicate quadruplets.
     For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
 
@@ -32,36 +32,40 @@ using std::sort;
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
-       vector<vector<int>> ret;
+       vector<vector<int>> result;
 
        sort(num.begin(), num.end());
        int n = num.size();
        for(int i = 0; i < n - 3; ++i) {
            // skip duplicates
-           if (i > 0 && num[i] == num[i - 1]) continue;
+           if (i > 0 && num[i] == num[i - 1])
+               continue;
 
            for (int j = i + 1; j < n - 2; ++j) {
                // skip duplicates
-               if (j > i + 1 && num[j] == num[j-1]) continue;
+               if (j > i + 1 && num[j] == num[j-1])
+                   continue;
 
                int k = j + 1, l = n - 1;
                while (k < l) {
                    int sum = num[i] + num[j] + num[k] + num[l];
                    if (sum == target) {
-                       ret.push_back({num[i], num[j], num[k], num[l]});
+                       result.push_back({num[i], num[j], num[k], num[l]});
 
                        ++k; --l;
                        // skip duplicates
                        while (k < l && num[k] == num[k-1]) ++k;
                        while (k < l && num[l] == num[l+1]) --l;
                    }
-                   else if (sum < target) ++k;
-                   else --l;
+                   else if (sum < target)
+                       ++k;
+                   else
+                       --l;
                }
            }
        }
 
-       return ret;
+       return result;
     }
 };
 
