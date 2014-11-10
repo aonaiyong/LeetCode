9199d5cb699a8ccac794d2a062e716117e383541
aonaiyong <aonaiyong@gmail.com>
2014-11-10 16:53:55 +0800

    final version

--- BalancedBinaryTree.h
+++ BalancedBinaryTree.h
@@ -27,20 +27,18 @@ using std::max;
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        int height = 0;
-        return isBalanced(root, height);
+        int ht = 0;
+        return isBalanced(root, ht);
     }
 
-    bool isBalanced(TreeNode *root, int &height) {
+    bool isBalanced(TreeNode *root, int &ht) {
         if (!root) return true;
 
-        int leftHeight = 0, rightHeight = 0;
-        if (!isBalanced(root->left, leftHeight)) return false;
-        if (!isBalanced(root->right, rightHeight)) return false;
-        if (abs(leftHeight - rightHeight) > 1) return false;
-
-        height = 1 + max(leftHeight, rightHeight);
-        return true;
+        int lht = 0, rht = 0;
+        if (!isBalanced(root->left, lht)) return false;
+        if (!isBalanced(root->right, rht)) return false;
+        ht = 1 + max(lht, rht);
+        return abs(lht - rht) <= 1;
     }
 };
 
