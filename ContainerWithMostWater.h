665998bb2a2fdbdcf51e6667af5fcc0404c488b0
aonaiyong <aonaiyong@gmail.com>
2014-11-16 20:25:42 +0800

    final version

--- ContainerWithMostWater.h
+++ ContainerWithMostWater.h
@@ -35,18 +35,18 @@ using std::max;
 class Solution {
 public:
     int maxArea(vector<int> &height) {
-        int result = 0;
+        int area = 0;
         for (int i = 0, j = height.size() - 1; i < j; ) {
             if (height[i] < height[j]) {
-                result = max(result, height[i] * (j-i));
+                area = max(area, height[i] * (j-i));
                 ++i;
             }
             else {
-                result = max(result, height[j] * (j-i));
+                area = max(area, height[j] * (j-i));
                 --j;
             }
         }
-        return result;
+        return area;
     }
 };
 
