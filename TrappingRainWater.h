b4af1ab10a7dc0151b6570289ecba487640a6a4a
aonaiyong <aonaiyong@gmail.com>
2014-11-16 20:21:27 +0800

    final version

--- TrappingRainWater.h
+++ TrappingRainWater.h
@@ -10,7 +10,8 @@
  For example,
  Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
- Solution:
+ Solution:    1. One-pass solution.
+
  */
 
 #ifndef TRAPPINGRAINWATER_H_
@@ -25,6 +26,32 @@ using std::max; using std::min;
 class Solution {
 public:
     int trap(int A[], int n) {
+        return trap1P(A, n);
+    }
+
+    // One-pass solution
+    // Time: O(n), Space: O(1)
+    int trap1P(int A[], int n) {
+        int totalArea = 0, barArea = 0;
+        int minLevel = 0;
+        for (int lt = 0, rt = n - 1; lt <= rt; ) {
+            // update the total area
+            if (min(A[lt], A[rt]) > minLevel) {
+                totalArea += (min(A[lt], A[rt]) - minLevel) * (rt - lt + 1);
+                minLevel = min(A[lt], A[rt]);
+            }
+            // update the bar area, and advance the smaller bar
+            if (A[lt] < A[rt])
+                barArea += A[lt++];
+            else
+                barArea += A[rt--];
+        }
+        return totalArea - barArea;
+    }
+
+    // Two-pass solution
+    // Time: O(n), Space: O(1)
+    int trap2P(int A[], int n) {
         vector<int> maxLeft(n, A[0]);
         vector<int> maxRight(n, A[n-1]);
         for (int i = 1; i < n; ++i) {
@@ -32,10 +59,10 @@ public:
             maxRight[n-1-i] = max(maxRight[n-i], A[n-1-i]);
         }
 
-        int result = 0;
+        int area = 0;
         for (int i = 1; i < n - 1; ++i)
-            result += min(maxLeft[i], maxRight[i]) - A[i];
-        return result;
+            area += min(maxLeft[i], maxRight[i]) - A[i];
+        return area;
     }
 };
 
