f01752f91952122c267f1692629a03971f30b03d
aonaiyong <aonaiyong@gmail.com>
2014-11-17 21:37:22 +0800

    final version

---
+++ Pascal'sTriangleII.h
@@ -0,0 +1,27 @@
+/*
+ Author:      naiyong, aonaiyong@gmail.com
+ Date:        Nov 17, 2014
+ Problem:     Pascal's Triangle II
+ Difficulty:  2
+ Source:      https://github.com/leetcoders/LeetCode/blob/master/Pascal'sTriangleII.h
+ Notes:
+ Given an index k, return the kth row of the Pascal's triangle.
+
+ For example, given k = 3,
+ Return [1,3,3,1].
+
+ Note:
+ Could you optimize your algorithm to use only O(k) extra space?
+
+ Solution:    Pascal's triangle. http://en.wikipedia.org/wiki/Pascal's_triangle
+              Calculate from back to forth.
+ */
+
+#ifndef PASCAL_STRIANGLEII_H_
+#define PASCAL_STRIANGLEII_H_
+
+
+
+
+
+#endif /* PASCAL_STRIANGLEII_H_ */
