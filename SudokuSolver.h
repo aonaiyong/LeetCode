1bf04187fd3fa0ab27cdd961086747175d7938d7
aonaiyong <aonaiyong@gmail.com>
2014-11-20 20:45:21 +0800

    final version

--- SudokuSolver.h
+++ SudokuSolver.h
@@ -22,8 +22,7 @@ using std::vector;
 
 class Solution {
 private:
-    const int N = 9;
-    const int n = 3;
+    enum { N = 9, n = 3 };
 public:
     void solveSudoku(vector<vector<char> > &board) {
         if (board.empty() || board[0].empty()) return;
@@ -31,7 +30,7 @@ public:
         solveSudoku(board, 0, 0);
     }
 
-    bool solveSudoku(vector<vector<char>> &board, int row, int col) {
+    bool solveSudoku(vector<vector<char> > &board, int row, int col) {
         nextEmpty(board, row, col);
         if (row == N) return true;
 
@@ -39,7 +38,8 @@ public:
         getCandidates(board, row, col, candidates);
         for (int i = 0; i < N; ++i) {
             if (!candidates[i]) continue;
-            board[row][col] = i + '1';
+
+            board[row][col] = '1' + i;
             if (solveSudoku(board, row, col)) return true;
         }
         board[row][col] = '.';
@@ -47,22 +47,22 @@ public:
     }
 
     // find next empty cell
-    void nextEmpty(const vector<vector<char>> &board, int &row, int &col) {
-        while (row < N && board[row][col] != '.') {
+    void nextEmpty(const vector<vector<char> > &board, int &row, int &col) {
+        while (row != N && board[row][col] != '.') {
             col = (col + 1) % N;
             row = col ? row : row + 1;
         }
     }
 
     // compute all valid candidates for cell (row, col)
-    void getCandidates(const vector<vector<char>> &board, int row, int col, vector<bool> &candidates) {
+    void getCandidates(const vector<vector<char> > &board, int row, int col, vector<bool> &candidates) {
         int startRow = row/n * n, startCol = col/n * n;
         // iterate over N cells in current row, column, and box respectively
         for (int k = 0; k < N; ++k) {
             if (board[row][k] != '.') candidates[board[row][k] - '1'] = false;
             if (board[k][col] != '.') candidates[board[k][col] - '1'] = false;
             int i = startRow + k/n, j = startCol + k%n;
-            if (board[i][j] != '.')  candidates[board[i][j] - '1'] = false;
+            if (board[i][j] != '.') candidates[board[i][j] - '1'] = false;
         }
     }
 };
